62fdb84523ae766ac16a2d67ad4e967e4ca8b465
Mary Bellavia <sauerwein.mary@gmail.com>
2020-01-24 14:55:04 -0600

    missing cheese menu file?

---
+++ CheeseMVC/Models/CheeseMenu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace CheeseMVC.Models
+{
+
+    /* CheeseMenu object represents a join table of the two objects
+     * Cheese and Menu to create a many-to-many relationship
+     *  -- the MenuID and CheeseID act as our keys to access the
+     *  objects in other tables
+     */
+
+    public class CheeseMenu
+    {
+        public int MenuID { get; set; }
+        public Menu Menu { get; set; }
+
+        public int CheeseID { get; set; }
+        public Cheese Cheese { get; set; }
+    }
+}
